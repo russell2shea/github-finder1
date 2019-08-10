90bd9221870ed189478de5c6217f82edc171535c
Russell Shea <russell2shea@gmail.com>
2019-08-10 18:30:50 -0400

    added proptypes for type checking

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+// propTypes is used for type checking
+import PropTypes from "prop-types";
 
 export class Navbar extends Component {
+  // default props over written if passed in
+  static defaultProps = {
+    title: "Github Finder",
+    icon: "fab fa-github"
+  };
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired
+  };
   render() {
     return (
       <nav className="navbar bg-primary">
         <h1>
-          <i className="fab fa-github" /> Navbar
+          <i className={this.props.icon} /> {this.props.title}
         </h1>
       </nav>
     );
