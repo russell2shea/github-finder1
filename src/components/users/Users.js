3a27826bfed24a8c2dc876be0158eb88f239d4b8
Russell Shea <russell2shea@gmail.com>
2019-08-10 20:22:50 -0400

    added github api instead of static state

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,32 +2,10 @@ import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 export class Users extends Component {
-  state = {
-    users: [
-      {
-        id: "15874441",
-        login: "russell2shea",
-        avatar_url: "https://avatars3.githubusercontent.com/u/15874441?v=4",
-        html_url: "https://github.com/russell2shea"
-      },
-      {
-        id: "15874441",
-        login: "russell2shea",
-        avatar_url: "https://avatars3.githubusercontent.com/u/15874441?v=4",
-        html_url: "https://github.com/russell2shea"
-      },
-      {
-        id: "15874441",
-        login: "russell2shea",
-        avatar_url: "https://avatars3.githubusercontent.com/u/15874441?v=4",
-        html_url: "https://github.com/russell2shea"
-      }
-    ]
-  };
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
