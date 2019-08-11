f4bab0d89c7abd428eafb6df99d26b040a504896
Russell Shea <russell2shea@gmail.com>
2019-08-11 11:15:56 -0400

    updated search function to use git api to search for useres based on input fields state

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     users: [],
     loading: false
   };
+
   async componentDidMount() {
     this.setState({ loading: true });
     const res = await axios.get(
@@ -20,12 +21,22 @@ class App extends Component {
     this.setState({ users: res.data, loading: false });
   }
 
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${
+        process.env.REACT_APP_GITHUB_CLIENT_ID
+      }&client_secretd=${process.env.REACT_APP_GITHUB_SECRET_ID}`
+    );
+    this.setState({ users: res.data.items, loading: false });
+  };
+
   render() {
     return (
       <div className="App">
         <Navbar title="Github Finder" icon="fab fa-github" />
         <div className="container">
-          <Search />
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
