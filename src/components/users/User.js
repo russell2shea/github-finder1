65bd412835cad38c4c5760d8ff8fecac005b2336
Russell Shea <russell2shea@gmail.com>
2019-08-11 14:25:27 -0400

    updated user compnent with git information

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -18,6 +18,7 @@ export class User extends Component {
       avatar_url,
       location,
       bio,
+      company,
       blog,
       login,
       html_url,
@@ -34,6 +35,67 @@ export class User extends Component {
         <Link to="/" className="btn btn-light">
           Back to Search
         </Link>
+        Hireable: {""}
+        {hireable ? (
+          <i className="fas fa-check text-success" />
+        ) : (
+          <i className="fas fa-times-circle text-danger" />
+        )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              className="round-img"
+              style={{ width: "150px" }}
+              alt=""
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio:</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a
+              href={html_url}
+              className="btn btn-dark my-1"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="card text-center">
+          <div className="badge badge-light">Public Repos: {public_repos}</div>
+          <div className="badge badge-light">Public Gists: {public_gists}</div>
+        </div>
       </Fragment>
     );
   }
