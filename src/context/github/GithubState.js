f7fd861d0775a7f77e19aeee51c68abcfdeeee82
Russell Shea <russell2shea@gmail.com>
2019-08-25 13:11:18 -0400

    Setting up context workflow

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,48 @@
+import React, { useReducer } from "react";
+import axios from "axios";
+import GithubConext from "./gitHubContext";
+import GithubReducer from "./githubReducer";
+import {
+  SEARCH_USERS,
+  SET_LOADING,
+  CLEAR_USERS,
+  GET_USER,
+  GET_REPOS
+} from "../types";
+
+const GithubState = props => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false
+  };
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+  // Search Users
+
+  //Get User
+
+  // Get Repos
+
+  //Clear Users
+
+  // Set Loading
+
+  // make it available to entire app
+  return (
+    <GithubConext.Provider
+      value={{
+        users: state.users,
+        user: state.user,
+        repos: state.repos,
+        loading: state.loading
+      }}
+    >
+      {props.children}
+    </GithubConext.Provider>
+  );
+};
+
+export default GithubState;
