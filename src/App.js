ef7789cd3bea58401b22721ca95d5905409f4e64
Russell Shea <russell2shea@gmail.com>
2019-10-06 15:05:37 -0400

    added not found page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Alert from "./components/layout/Alert";
 import About from "./components/pages/About";
 import Home from "./components/pages/Home";
+import NotFound from "./components/pages/NotFound";
 import User from "./components/users/User";
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
@@ -22,6 +23,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
